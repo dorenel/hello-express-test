d846c09e3ef27f681183e17362aba72860c332df
Dorene Lau <dorene_lau@Hotmail.com>
2022-07-15 22:33:27 -0700

    added static page

--- http_server.js
+++ http_server.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const app = express();
 
+app.use(express.static('public'));
+
 app.get('/hello', (req, res) => {
     res.send("Hello World");
 });
 
 //comment this out for jest testing
-//app.listen(3000, () => {
-//    console.log("Listening on port 3000...");
-//});
-
+app.listen(3000, () => {
+    console.log("Listening on port 3000...");
+});
 
 module.exports = app;
