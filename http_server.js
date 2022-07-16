d390c776fb9a907d5d2c5ba478028d27d8633530
Dorene Lau <dorene_lau@Hotmail.com>
2022-07-15 22:21:38 -0700

    change to be hosted

--- http_server.js
+++ http_server.js
@@ -5,10 +5,10 @@ app.get('/hello', (req, res) => {
     res.send("Hello World");
 });
 
-/* comment this out for jest testing
+//comment this out for jest testing
 app.listen(3000, () => {
     console.log("Listening on port 3000...");
 });
-*/
+
 
 module.exports = app;
